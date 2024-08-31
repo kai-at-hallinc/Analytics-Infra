754e4690cfededa0ce1ab001d14a98c22153515b
kai-at-hallinc <kai@hallinc.onmicrosoft.com>
2024-08-31 14:12:09 +0300

    moving on. to do test storage account

--- deploy/Storage.Tests.ps1
+++ deploy/Storage.Tests.ps1
@@ -7,7 +7,7 @@ param(
 Describe 'storage account' {
 
 It 'Reaches storage over the internet' {
-      $request = [System.Net.WebRequest]::Create("https://$HostName/")
+      $request = [System.Net.WebRequest]::Create("https://$HostName.dfs.core.windows.net/")
       $request.AllowAutoRedirect = $false
       $request.GetResponse().StatusCode |
         Should -Be 200 -Because "the storage is reachable over public internet"
